f52044e71f553c323054f784ecec3a69100ff1e3
Nahiankhondokar <info.nahian13@gmail.com>
2026-02-15 00:38:17 +0600

    wip: user-profile api response type added

--- app/(portfolio)/type/type.ts
+++ app/(portfolio)/type/type.ts
@@ -1,8 +1,65 @@
 // --- Types ---
 export type Section = "home" | "about" | "portfolio" | "contact";
-export type Home  = {
-    name : string,
-    bio: string | null,
-    subtitle: string | null,
-    image: string | null
+export type Home = Pick<UserInfo, "name" | "subtitle" | "image" | "bio">
+
+
+export interface UserInfo {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+    bio: string | null;
+    location: string | null;
+    website: string | null;
+    image: string | null;
+    phone: string | null;
+    socials: Social[];
+    nationality: string | null;
+    subtitle: string | null;
+    address: string | null;
+    job_type: string | null;
+    exp_year: string | null;
+    clients: string | null;
+    projects: string | null;
+    awards: string | null;
+    expertise: Expertise[];
+    experience: Experience[];
+    certificate: Certificate[];
 }
+
+
+
+export interface Expertise {
+    id: number;
+    name: string;
+    description: string;
+    status: boolean;
+    progress: string; // "10%"
+}
+
+export interface Experience {
+    id: number;
+    title: string;
+    description: string;
+    position: string;
+    duration: string;
+    company: string;
+    start_date: string; // ISO date
+    end_date: string;   // ISO date
+    media: string | null;
+}
+
+export interface Social {
+    id: number;
+    platform: string;
+    url: string;
+}
+
+export interface Certificate {
+    id: number;
+    title: string;
+    issuer: string;
+    issue_date: string;
+    credential_url: string | null;
+}
+
